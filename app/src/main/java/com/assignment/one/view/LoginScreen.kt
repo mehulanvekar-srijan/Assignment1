55788c45119871785d60132ba9dabbbf20889a80
mehulanvekar-srijan <mehul.anvekar@srijan.net>
2022-09-22 19:52:52 +0530

    Reduced recomposition of whole LoginScreen

--- app/src/main/java/com/assignment/one/view/LoginScreen.kt
+++ app/src/main/java/com/assignment/one/view/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.assignment.one.view
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,7 +26,6 @@ import com.assignment.one.R
 import com.assignment.one.viewmodel.LoginViewModel
 import com.assignment.one.ui.theme.*
 
-
 @Composable
 fun LogInScreenTheme(context: Context, navController: NavHostController, loginViewModel: LoginViewModel) {
 
@@ -37,82 +37,104 @@ fun LogInScreenTheme(context: Context, navController: NavHostController, loginVi
     ) {
 
         //User Name
-        Row(modifier = Modifier,
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Column(modifier = Modifier.padding(10.dp)){
-                Text(text = "User Name", color = LoginText, textAlign = TextAlign.Center)
-            }
-            Column(modifier = Modifier.padding(10.dp)){
-                OutlinedTextField(
-                    value = loginViewModel.userNameState.value,
-                    onValueChange = {
-                        loginViewModel.onUserNameValueChange(it)
-                    },
-                    label = { Text(text = "Enter user name") }
-                )
-            }
-        }
+        DrawUserName(loginViewModel = loginViewModel)
 
         //Password
-        Row(modifier = Modifier,
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Column(modifier = Modifier.padding(10.dp)){
-                Text(text = "Password ", color = LoginText, textAlign = TextAlign.Center)
-            }
-            Column(modifier = Modifier.padding(10.dp)){
-                OutlinedTextField(
-                    value = loginViewModel.password.value,
-                    onValueChange = {
-                        loginViewModel.onPasswordValueChange(it)
-                    },
-                    visualTransformation = if(loginViewModel.passwordVisibility.value) VisualTransformation.None else PasswordVisualTransformation(),
-                    label = { Text(text = "Enter password") },
-                    trailingIcon = {
-                      IconButton(onClick = { loginViewModel.onPasswordVisibilityChange() }) {
-                          if(loginViewModel.passwordVisibility.value)
-                              Icon(imageVector = Icons.Rounded.Lock, contentDescription = "", tint = Color.Red)
-                          else
-                              Icon(imageVector = Icons.Rounded.Lock, contentDescription = "", tint = Purple700)
-                      }
-                    },
-                )
-            }
-        }
+        DrawPassword(loginViewModel = loginViewModel)
 
-        Button(
-            onClick = { loginViewModel.onClickButton(context,navController) },
-            shape = RoundedCornerShape(50),
-            modifier = Modifier
-                .fillMaxWidth(0.5F)
-                .padding(horizontal = 20.dp)
-                .padding(top = 5.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = LoginButtonBackground,
-                contentColor = LoginButtonText
-            ),
-        ) { Text(text = "Lets Go") }      // Button : Lets Go
+        //LetsGo Button
+        LetsGoButton(context,navController,loginViewModel)
 
         if(loginViewModel.openDialog.value){
-            AlertDialog(
-                onDismissRequest = {
-                    loginViewModel.onDialogResponseChange(false)
+            DrawAlertDialog(loginViewModel = loginViewModel)
+        }
+    }
+}
+
+@Composable
+fun DrawUserName(loginViewModel: LoginViewModel) {
+    Row(modifier = Modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Column(modifier = Modifier.padding(10.dp)){
+            Text(text = "User Name", color = LoginText, textAlign = TextAlign.Center)
+        }
+        Column(modifier = Modifier.padding(10.dp)){
+            OutlinedTextField(
+                value = loginViewModel.userNameState.value,
+                onValueChange = {
+                    loginViewModel.onUserNameValueChange(it)
                 },
-                title = {
-                    Text(text = "Hello ${loginViewModel.userNameState.value}")
+                label = { Text(text = "Enter user name") }
+            )
+        }
+    }
+}
+
+@Composable
+fun DrawPassword(loginViewModel: LoginViewModel) {
+    Row(modifier = Modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Column(modifier = Modifier.padding(10.dp)){
+            Text(text = "Password ", color = LoginText, textAlign = TextAlign.Center)
+        }
+        Column(modifier = Modifier.padding(10.dp)){
+            OutlinedTextField(
+                value = loginViewModel.password.value,
+                onValueChange = {
+                    loginViewModel.onPasswordValueChange(it)
                 },
-                text = {
-                    Text("Incorrect username or password")
-                },
-                confirmButton = {
-                    Button(onClick = {
-                        loginViewModel.onDialogResponseChange(false)
-                    }) { Text("Ok") }
+                visualTransformation = if(loginViewModel.passwordVisibility.value) VisualTransformation.None else PasswordVisualTransformation(),
+                label = { Text(text = "Enter password") },
+                trailingIcon = {
+                    IconButton(onClick = { loginViewModel.onPasswordVisibilityChange() }) {
+                        if(loginViewModel.passwordVisibility.value)
+                            Icon(imageVector = Icons.Rounded.Lock, contentDescription = "", tint = Color.Red)
+                        else
+                            Icon(imageVector = Icons.Rounded.Lock, contentDescription = "", tint = Purple700)
+                    }
                 },
             )
         }
     }
+}
+
+@Composable
+fun LetsGoButton(context: Context, navController: NavHostController, loginViewModel: LoginViewModel) {
+    Button(
+        onClick = { loginViewModel.onClickButton(context,navController) },
+        shape = RoundedCornerShape(50),
+        modifier = Modifier
+            .fillMaxWidth(0.5F)
+            .padding(horizontal = 20.dp)
+            .padding(top = 5.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = LoginButtonBackground,
+            contentColor = LoginButtonText
+        ),
+    ) { Text(text = "Lets Go") }      // Button : Lets Go
+
+}
+
+@Composable
+fun DrawAlertDialog(loginViewModel: LoginViewModel) {
+    AlertDialog(
+        onDismissRequest = {
+            loginViewModel.onDialogResponseChange(false)
+        },
+        title = {
+            Text(text = "Hello ${loginViewModel.userNameState.value}")
+        },
+        text = {
+            Text("Incorrect username or password")
+        },
+        confirmButton = {
+            Button(onClick = {
+                loginViewModel.onDialogResponseChange(false)
+            }) { Text("Ok") }
+        },
+    )
 }
